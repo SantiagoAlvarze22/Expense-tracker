6d12106ba5e80d98f657c7086ef94aa5680c6341
Santiago Alvarez Olivera <sao2207@gmail.com>
2020-10-15 10:05:24 -0500

    display balance, income and expense

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const balance = document.getElementById('balance');
-const money_plus = document.getElementById('money_plus');
-const money_minus = document.getElementById('money_minus');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
 const list = document.getElementById('list');
 const form = document.getElementById('form');
 const text = document.getElementById('text');
@@ -34,6 +34,27 @@ function addTransactionDOM(transaction) {
 
   list.appendChild(item)
 }
+
+//update the balance, income and xpense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (amounts
+    .filter(item => item < 0)
+    .reduce((acc, item) => (acc += item), 0)
+    * -1).toFixed(2)
+  balance.innerHTML = `$${total}`
+  money_plus.innerHTML = `$${income}`
+  money_minus.innerHTML = `$${expense}`
+}
+updateValues()
 
 //init app 
 
